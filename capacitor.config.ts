6a8c101ae51397f6427d95f74b9e8696655c9bc8
Peter karanja <peterkaranja60@gmail.com>
2022-03-04 14:39:28 +0300

    Initial commit
    first app beginner

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'first_app',
+  appId: 'com.example.app',
+  appName: 'first-app',
   webDir: 'www',
   bundledWebRuntime: false
 };
